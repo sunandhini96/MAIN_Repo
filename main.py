15340779071fbfc714d3a397248050a8c318a260
sunandhini96 <63030539+sunandhini96@users.noreply.github.com>
2023-07-29 20:12:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,29 +19,7 @@ from models.resnet import *
 # loading the dataset
 mean=(0.4914, 0.4822, 0.4465)
 std=(0.2470, 0.2435, 0.2616)
-def train_transform_function(mean,std):
-    train_transform = A.Compose([A.Normalize(mean,std,always_apply=True),
-                                 #A.PadIfNeeded(min_height=40,min_width=40,always_apply=True),
-                                 A.RandomCrop(height=32,width=32,padding=4, always_apply=True),
-                                 #A.HorizontalFlip(),
-                                 #A.ShiftScaleRotate(shift_limit=0.05, scale_limit=0.05,rotate_limit=15,p=0.5),
-                                 A.CoarseDropout(max_holes=1,max_height=16,max_width=16, min_holes=1, min_height=16,min_width=16, fill_value=mean, mask_fill_value = None),
-                                 ToTensorV2()
 
-                                       ])
-    return lambda img:train_transform(image=np.array(img))["image"]
-# Test Phase transformations
-def test_transform_function(mean,std):
-      test_transform = A.Compose([
-                                            #  transforms.Resize((28, 28)),
-                                            #  transforms.ColorJitter(brightness=0.10, contrast=0.1, saturation=0.10, hue=0.1),
-
-                                            #transforms.ToTensor(),
-                                            A.Normalize(mean,std),
-                                            ToTensorV2()
-                                            #transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
-                                            ])
-      return lambda img:test_transform(image=np.array(img))["image"]
 train = datasets.CIFAR10(root='./data', train=True,
                                         download=True, transform=train_transform_function(mean,std))
 test = datasets.CIFAR10(root='./data', train=False,
