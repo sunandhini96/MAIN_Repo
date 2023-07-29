3141e602caadc72b860a8600476ea9b1310aabf6
sunandhini96 <63030539+sunandhini96@users.noreply.github.com>
2023-07-29 20:10:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,271 @@
+from torchvision import datasets, transforms
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import albumentations as A
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.nn as nn
+from torchsummary import summary
+from tqdm import tqdm
+from torch.optim.lr_scheduler import StepLR
+import torch.optim as optim
+
+from models import resnet
+from utils import *
+
+from models.resnet import *
+
+# loading the dataset
+mean=(0.4914, 0.4822, 0.4465)
+std=(0.2470, 0.2435, 0.2616)
+def train_transform_function(mean,std):
+    train_transform = A.Compose([A.Normalize(mean,std,always_apply=True),
+                                 #A.PadIfNeeded(min_height=40,min_width=40,always_apply=True),
+                                 A.RandomCrop(height=32,width=32,padding=4, always_apply=True),
+                                 #A.HorizontalFlip(),
+                                 #A.ShiftScaleRotate(shift_limit=0.05, scale_limit=0.05,rotate_limit=15,p=0.5),
+                                 A.CoarseDropout(max_holes=1,max_height=16,max_width=16, min_holes=1, min_height=16,min_width=16, fill_value=mean, mask_fill_value = None),
+                                 ToTensorV2()
+
+                                       ])
+    return lambda img:train_transform(image=np.array(img))["image"]
+# Test Phase transformations
+def test_transform_function(mean,std):
+      test_transform = A.Compose([
+                                            #  transforms.Resize((28, 28)),
+                                            #  transforms.ColorJitter(brightness=0.10, contrast=0.1, saturation=0.10, hue=0.1),
+
+                                            #transforms.ToTensor(),
+                                            A.Normalize(mean,std),
+                                            ToTensorV2()
+                                            #transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+                                            ])
+      return lambda img:test_transform(image=np.array(img))["image"]
+train = datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=train_transform_function(mean,std))
+test = datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=test_transform_function(mean,std))
+SEED = 1
+
+# CUDA?
+cuda = torch.cuda.is_available()
+print("CUDA Available?", cuda)
+
+# For reproducibility
+torch.manual_seed(SEED)
+
+if cuda:
+    torch.cuda.manual_seed(SEED)
+
+# dataloader arguments - something you'll fetch these from cmdprmt
+dataloader_args = dict(shuffle=True, batch_size=512, num_workers=1, pin_memory=True) if cuda else dict(shuffle=True, batch_size=64)
+
+# train dataloader
+train_loader = torch.utils.data.DataLoader(train, **dataloader_args)
+
+# test dataloader
+test_loader = torch.utils.data.DataLoader(test, **dataloader_args)
+
+# exp = datasets.CIFAR10('./data', train=True, download=True)
+# exp_data = exp.data
+
+# use_cuda = torch.cuda.is_available()
+# device = torch.device("cuda" if use_cuda else "cpu")
+print('==> Building model..')
+
+model =  ResNet18().to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+scheduler = StepLR(optimizer, step_size=6, gamma=0.1)
+
+EPOCHS = 20
+
+train_losses = []
+test_losses = []
+train_acc = []
+test_acc = []
+
+criterion = nn.CrossEntropyLoss()
+def train(model, device, train_loader, criterion, scheduler, optimizer, use_l1=False, lambda_l1=0.01):
+    """Function to train the model
+
+    Args:
+        model (instance): torch model instance of defined model
+        device (str): "cpu" or "cuda" device to be used
+        train_loader (instance): Torch Dataloader instance for trainingset
+        criterion (instance): criterion to used for calculating the loss
+        scheduler (function): scheduler to be used
+        optimizer (function): optimizer to be used
+        use_l1 (bool, optional): L1 Regularization method set True to use . Defaults to False.
+        lambda_l1 (float, optional): Regularization parameter of L1. Defaults to 0.01.
+
+    Returns:
+        float: accuracy and loss values
+    """
+    model.train()
+    pbar = tqdm(train_loader)
+    lr_trend = []
+    correct = 0
+    processed = 0
+    train_loss = 0
+
+    for batch_idx, (data, target) in enumerate(pbar):
+        # get samples
+        data, target = data.to(device), target.to(device)
+
+        # Init
+        optimizer.zero_grad()
+        # In PyTorch, we need to set the gradients to zero before starting to do backpropragation because PyTorch 
+        # accumulates the gradients on subsequent backward passes. Because of this, when you start your training loop, 
+        # ideally you should zero out the gradients so that you do the parameter update correctly.
+
+        # Predict
+        y_pred = model(data)
+        # Calculate loss
+        loss = criterion(y_pred, target)
+
+        l1=0
+        if use_l1:
+            for p in model.parameters():
+                l1 = l1 + p.abs().sum()
+        loss = loss + lambda_l1*l1
+
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+        # updating LR
+        if scheduler:
+            if not isinstance(scheduler, torch.optim.lr_scheduler.ReduceLROnPlateau):
+                scheduler.step()
+                lr_trend.append(scheduler.get_last_lr()[0])
+
+        train_loss += loss.item()
+
+        # Update pbar-tqdm
+        pred = y_pred.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+        correct += pred.eq(target.view_as(pred)).sum().item()
+        processed += len(data)
+        
+
+        pbar.set_description(desc= f'Batch_id={batch_idx} Loss={train_loss/(batch_idx + 1):.5f} Accuracy={100*correct/processed:0.2f}%')
+    return 100*correct/processed, train_loss/(batch_idx + 1), lr_trend
+
+
+def test(model, device, test_loader, criterion):
+    """put model in eval mode and test it
+
+    Args:
+        model (instance): torch model instance of defined model
+        device (str): "cpu" or "cuda" device to be used
+        test_loader (instance): Torch Dataloader instance for testset
+        criterion (instance): criterion to used for calculating the loss
+
+    Returns:
+        float: accuracy and loss values
+    """
+    model.eval()
+    test_loss = 0
+    correct = 0
+    #iteration = len(test_loader.dataset)// test_loader.batch_size
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += criterion(output, target).item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+    return 100. * correct / len(test_loader.dataset), test_loss
+
+
+    
+
+ 
+def save_model(model, epoch, optimizer, path):
+    """Save torch model in .pt format
+
+    Args:
+        model (instace): torch instance of model to be saved
+        epoch (int): epoch num
+        optimizer (instance): torch optimizer
+        path (str): model saving path
+    """
+    state = {
+        'epoch': epoch,
+        'state_dict': model.state_dict(),
+        'optimizer': optimizer.state_dict()
+    }
+    torch.save(state, path)
+
+
+def fit_model(net, optimizer, criterion, device, NUM_EPOCHS,train_loader, test_loader, use_l1=False, scheduler=None, save_best=False):
+    """Fit the model
+
+    Args:
+        net (instance): torch model instance of defined model
+        optimizer (function): optimizer to be used
+        criterion (instance): criterion to used for calculating the loss
+        device (str): "cpu" or "cuda" device to be used
+        NUM_EPOCHS (int): number of epochs for model to be trained
+        train_loader (instance): Torch Dataloader instance for trainingset
+        test_loader (instance): Torch Dataloader instance for testset
+        use_l1 (bool, optional): L1 Regularization method set True to use. Defaults to False.
+        scheduler (function, optional): scheduler to be used. Defaults to None.
+        save_best (bool, optional): If save best model to model.pt file, paramater validation loss will be monitered
+
+    Returns:
+        (model, list): trained model and training logs
+    """
+    training_acc, training_loss, testing_acc, testing_loss = list(), list(), list(), list()
+    lr_trend = []
+    if save_best:
+        min_val_loss = np.inf
+        save_path = 'model.pt'
+
+    for epoch in range(1,NUM_EPOCHS+1):
+        print("EPOCH: {} (LR: {})".format(epoch, optimizer.param_groups[0]['lr']))
+        
+        train_acc, train_loss, lr_hist = train(
+            model=net, 
+            device=device, 
+            train_loader=train_loader, 
+            criterion=criterion ,
+            optimizer=optimizer, 
+            use_l1=use_l1, 
+            scheduler=scheduler
+        )
+        test_acc, test_loss = test(net, device, test_loader, criterion)
+        # update LR
+        if scheduler:
+            if isinstance(scheduler, torch.optim.lr_scheduler.ReduceLROnPlateau):
+                scheduler.step(test_loss)
+        
+        if save_best:
+            if test_loss < min_val_loss:
+                print(f'Valid loss reduced from {min_val_loss:.5f} to {test_loss:.6f}. checkpoint created at...{save_path}\n')
+                save_model(net, epoch, optimizer, save_path)
+                min_val_loss = test_loss
+            else:
+                print(f'Valid loss did not inprove from {min_val_loss:.5f}\n')
+        else:
+            print()
+
+        training_acc.append(train_acc)
+        training_loss.append(train_loss)
+        testing_acc.append(test_acc)
+        testing_loss.append(test_loss)
+        lr_trend.extend(lr_hist)    
+
+    if scheduler:   
+        return net, (training_acc, training_loss, testing_acc, testing_loss, lr_trend)
+    else:
+        return net, (training_acc, training_loss, testing_acc, testing_loss)
+    
 
