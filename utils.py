2fb7cf15cba9a5a696e49b0ad0e175e776398a9b
sunandhini96 <63030539+sunandhini96@users.noreply.github.com>
2023-07-29 20:45:38 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -30,7 +30,7 @@ from pytorch_grad_cam.utils.image import show_cam_on_image
 def train_transform_function(mean,std):
     train_transform = A.Compose([A.Normalize(mean,std,always_apply=True),
                                  #A.PadIfNeeded(min_height=40,min_width=40,always_apply=True),
-                                 A.RandomCrop(height=32,width=32,padding=4, always_apply=True),
+                                 A.RandomCrop(height=32,width=32, always_apply=True),
                                  #A.HorizontalFlip(),
                                  #A.ShiftScaleRotate(shift_limit=0.05, scale_limit=0.05,rotate_limit=15,p=0.5),
                                  A.CoarseDropout(max_holes=1,max_height=16,max_width=16, min_holes=1, min_height=16,min_width=16, fill_value=mean, mask_fill_value = None),
